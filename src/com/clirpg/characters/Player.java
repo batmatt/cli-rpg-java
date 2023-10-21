c5d0d9daab1b1553d48242e856ddbabff8ce1758
osullb38 <brian.osullivan38@mail.dcu.ie>
2023-10-21 11:31:05 +0200

    added character classes. methods incomplete

--- src/com/clirpg/characters/Player.java
+++ src/com/clirpg/characters/Player.java
@@ -1,5 +1,20 @@
-package src.com.clirpg.characters;
-
-public class Player {
+class Player extends Entity implements Combat{
+    final static boolean friendly = true;
+    String job = "Warrior";
     
+    Player(String name)
+    {
+        super(name);
+        this.health = 100;
+    }
+
+    public void combat()
+    {
+        System.out.println("start combat");
+    }
+
+    public String toString()
+    {
+        return this.name + " " + this.job; 
+    }
 }
