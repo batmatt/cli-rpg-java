9f8230ba620c7b0b384647197c04859336357f15
SimH0et <148080841+SimH0et@users.noreply.github.com>
2023-10-27 13:22:43 +0200

    Update talking in Arena

--- src/com/clirpg/locations/Arena.java
+++ src/com/clirpg/locations/Arena.java
@@ -2,14 +2,24 @@ package src.com.clirpg.locations;
 
 import java.util.Scanner;
 
+import src.com.clirpg.characters.Civillian;
+import src.com.utils.ConsoleColors;
+
 public class Arena implements Visit{
+    
+    private Civillian trainer;
+
+    public Arena() {
+        trainer = new Civillian("Bertrand", "Trainer");
+    }
     
     public void visit(){
 
-        System.out.println("Welcome to the arena!");
+        System.out.println("\n");
+        System.out.println(ConsoleColors.PURPLE_BOLD + trainer.toString() + ": Welcome to the arena!" + ConsoleColors.RESET);
 
         Scanner lvlScan = new Scanner(System.in);
-        System.out.println("At what level do you want to start?");
+        System.out.println(ConsoleColors.PURPLE + trainer.toString() + ": At what level do you want to start?" + ConsoleColors.RESET);
 
         int levelArena = lvlScan.nextInt();
 
@@ -18,15 +28,17 @@ public class Arena implements Visit{
         String contFight = "y";
         Scanner contScan = new Scanner(System.in);
         while(contFight.equals("y")){
-            System.out.println("Do you want to continue fighting?");
+            System.out.println(ConsoleColors.PURPLE + trainer.toString() + ": Good fight! Do you want to continue fighting?" + ConsoleColors.RESET);
             System.out.println("If yes type y otherwise n");
             contFight = contScan.nextLine();
 
             switch(contFight){
                 case "y":   levelArena++; 
-                            System.out.println("The fight continues with the arena level " + levelArena); 
+                            System.out.println(ConsoleColors.PURPLE + trainer.toString() + ": The fight continues with the arena level " + levelArena + ConsoleColors.RESET); 
                             /*  start fight with power level: levelArena;*/ break;
-                case "n":   contFight = "n"; break;
+                case "n":   contFight = "n"; 
+                            System.out.println(ConsoleColors.PURPLE_BOLD + trainer.toString() + ": This was a nice fighting session. I look forward to seeing you in the arena again." + ConsoleColors.RESET);
+                            break;
                 default:    System.out.println("This is not a valid option. Try again."); 
                             contFight = "y";
             }
